6e9067004ef60945269eb3d6e2618fb57f6f0963
nolan-likescoffee <benjinolann@gmail.com>
2026-03-03 20:14:18 +0530

    Add files via upload

---
+++ Final_Class.java
@@ -0,0 +1,11 @@
+public final class Final_Class {
+
+    public void display() {
+        System.out.println("This is a final class.");
+    }
+
+    public static void main(String[] args) {
+        Final_Class obj = new Final_Class();
+        obj.display();
+    }
+}
