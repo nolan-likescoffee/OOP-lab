64e768f5c2ed26b599d263a45d78759f4b8523a7
nolan-likescoffee <benjinolann@gmail.com>
2026-03-03 20:24:24 +0530

    car2.java

--- car2.java
+++ car2.java
@@ -3,5 +3,9 @@ class Car {
     private int speed;
     private void accelerate() {
         speed += 10;
+    public statid void main(string[] args){
+        System.print.outln("This is a private modifier example")
     }
+    }
+
 }
